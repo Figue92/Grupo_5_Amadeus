85310eeb551295c4bfdfde1cddf9819d756fd5bd
Matias - YOMYMOY <matdnunez@gmail.com>
2023-02-09 02:07:37 -0300

    cambia formAdd, crea método store para agregar productos, agrega json de users

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,41 +1,62 @@
-const fs= require('fs');
-const path= require('path');
-const productosFilePath= path.join(__dirname, '../data/productos.json');
-const productos= JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
+const fs = require('fs');
+const path = require('path');
+const productosFilePath = path.join(__dirname, '../data/productos.json');
+const productos = JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
 
 
 module.exports = {
 
-    filter : (req, res) => {
-       
-        return res.render('productos/filtrarProductos',{
+    filter: (req, res) => {
+
+        return res.render('productos/filtrarProductos', {
             title: "Lista de Productos",
             productos
         })
     },
 
-    detail : (req, res) =>{
+    detail: (req, res) => {
         const { id } = req.params;
 
         const producto = productos.find(producto => producto.id === +id)
-       
-       return res.render('productos/productDetail',{
+
+        return res.render('productos/productDetail', {
             title: "Detalle del curso",
             ...producto
         })
-      },
-      cart: (req, res) => {
-        return res.render('productos/carrito',{
-            
-            title : "Carrito",
+    },
+    cart: (req, res) => {
+        return res.render('productos/carrito', {
+
+            title: "Carrito",
 
         })
-      },
-      add : (req,res) => {
+    },
+    add: (req, res) => {
         return res.render('productos/formAdd');
     },
-    edit : (req,res) => {
+
+    store: (req, res) => {
+        const { name, price, description, category, marca, discount, novedad } = req.body;
+        const newProduct = {
+            id: productos.length + 1,
+            name: name.trim(),
+            price: +price,
+            description: description.trim(),
+            category: category,
+            marca: marca,
+            discount: +discount,
+            novedad: novedad,
+            image: null
+        }
+        productos.push(newProduct);
+
+        fs.writeFileSync(productosFilePath,JSON.stringify(productos, null, 3), 'UTF-8');
+
+        return res.redirect('/');
+    },
+
+    edit: (req, res) => {
         return res.render('productos/formEdit');
     }
 
-    }
+}
--- routes/productos.js
+++ routes/productos.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 
-const{detail, filter, cart, add, edit}= require('../controllers/productosController')
+const{detail, filter, cart, add, edit, store}= require('../controllers/productosController')
 
 router.get('/carrito', cart);
 router.get('/productDetail/:id', detail );
 router.get('/filtrarProductos', filter);
 router.get('/add', add);
-router.get('/edit', edit)
+router.post('/', store);
+router.get('/edit', edit);
 
 module.exports = router;
