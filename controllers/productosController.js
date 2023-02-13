866f0c3273298e6e56f6fda09b8181f9af176f9f
Matias - YOMYMOY <matdnunez@gmail.com>
2023-02-12 23:13:27 -0300

    list, edit a medias

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -15,6 +15,14 @@ module.exports = {
         })
     },
 
+    list : (req,res) => {
+        const productosFilePath = path.join(__dirname, '../data/productos.json');
+        const productos = JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
+        return res.render('productos/productos', {
+            productos
+        })
+    },
+
     detail: (req, res) => {
         const { id } = req.params;
 
@@ -57,7 +65,39 @@ module.exports = {
     },
 
     edit: (req, res) => {
-        return res.render('productos/formEdit');
+        const {id} = req.params;
+		const producto = productos.find(producto => producto.id === +id);
+		return res.render('productos/formEdit', {
+			...producto
+		});
+    },
+    update : (req,res) => {
+        const {id} = req.params;
+		const { name, price, description, category, marca, discount, novedad } = req.body;
+		const producto = productos.find(producto => producto.id === +id);
+		
+		const productoUpdate = {
+			id : +id,
+			name : name.trim(),
+            price : +price,
+			description : description.trim(),
+			category,
+            marca,
+			discount : +discount,
+            novedad,
+			image : null,
+		};
+
+		const productosUpdate = productos.map(producto => {
+			if(producto.id === +id){
+				return productoUpdate;
+			}
+			return producto;
+		});
+
+		fs.writeFileSync(productosFilePath,JSON.stringify(productosUpdate, null, 3), 'UTF-8');
+
+  		return res.redirect('/productos/productDetail/' + id);
     }
 
 }
--- routes/productos.js
+++ routes/productos.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 
-const{detail, filter, cart, add, edit, store}= require('../controllers/productosController')
+const{detail, filter, cart, add, edit, store, update, list}= require('../controllers/productosController')
 
 router.get('/carrito', cart);
 router.get('/productDetail/:id', detail );
 router.get('/filtrarProductos', filter);
 router.get('/add', add);
 router.post('/', store);
-router.get('/edit', edit);
+router.get('/edit/:id', edit);
+router.put('/edit/:id', update);
+router.get('/productos', list)
 
 module.exports = router;
