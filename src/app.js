29c7c01004d4298e2735e33ea12bb6c05a09d586
Matias - YOMYMOY <matdnunez@gmail.com>
2023-03-08 14:11:08 -0300

    crea carpeta src y mueve el contenido

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(methodOverride('_method'));
 app.use(session({secret : "amdGrupo5", resave : true, saveUninitialized : true}))
 
