a6c33d624dc9942ab4758479a1538355851dc5ca
Matias - YOMYMOY <matdnunez@gmail.com>
2023-03-08 02:51:57 -0300

    instala dependencias a utilizar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const methodOverride = require('method-override');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+app.use(session({secret : "amdGrupo5", resave : true, saveUninitialized : true}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
