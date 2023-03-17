2a1ddb8710c45b5f2b1b87fe5ef7d556b9499636
Matias - YOMYMOY <matdnunez@gmail.com>
2023-03-17 09:46:47 -0300

    agrega middleware de admin a las rutas necesarias

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -4,17 +4,18 @@ const router = express.Router();
 
 const{detail, filter, cart, add, edit, store, update, list, remove}= require('../controllers/productosController')
 const checkUserLogin = require('../middlewares/checkUserLogin');
+const checkUserAdmin = require('../middlewares/checkUserAdmin');
 const {uploadImage}= require('../middlewares/upload')
 
 router
     .get('/carrito', cart)
     .get('/productDetail/:id', detail )
     .get('/filtrarProductos', filter)
-    .get('/add', checkUserLogin, add)
-    .post('/', uploadImage.array('images'), store)
-    .get('/edit/:id', edit)
-    .put('/edit/:id', uploadImage.array('images'), update)
+    .get('/add', checkUserAdmin, add)
+    .post('/', uploadImage.array('images'), checkUserAdmin, store)
+    .get('/edit/:id', checkUserAdmin, edit)
+    .put('/edit/:id', uploadImage.array('images'), checkUserAdmin, update)
     .get('/productos', list)
-    .delete('/delete/:id', remove)
+    .delete('/delete/:id', checkUserAdmin, remove)
    
 module.exports = router;
