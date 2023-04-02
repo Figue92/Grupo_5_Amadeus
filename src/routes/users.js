b9fe1941f9f3a04a956742d7176fcc07c2193b2c
Matias - YOMYMOY <matdnunez@gmail.com>
2023-04-02 17:21:27 -0300

    añade index a ruta de validations en users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,7 @@ const checkUser = require('../middlewares/checkUser');
 const checkUserAdmin = require('../middlewares/checkUserAdmin');
 const checkUserLogin = require('../middlewares/checkUserLogin');
 const { uploadPerfil } = require('../middlewares/uploadPerfil');
-const { registerUserValidator, loginUserValidator } = require('../validations');
+const { registerUserValidator, loginUserValidator } = require('../validations/index');
 
 router.get('/register', checkUser, register)
 router.get('/login', checkUser, login)
